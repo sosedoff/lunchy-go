877196416d75912693fe3a5ba446c31318cdc896
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-11-29 22:52:19 -0600

    Print usage

--- lunchy.go
+++ lunchy.go
@@ -2,8 +2,23 @@ package main
 
 import(
   "fmt"
+  "os"
 )
 
+const(
+  LUNCHY_VERSION = "0.1.0"
+)
+
+func printUsage() {
+  fmt.Printf("Lunchy %s, the friendly launchctl wrapper\n", LUNCHY_VERSION)
+  fmt.Println("Usage: lunchy [start|stop|restart|list|status|install|show|edit] [options]")
+}
+
 func main() {
-  fmt.Println("Lunchy")
+  args := os.Args
+
+  if (len(args) == 1) {
+    printUsage()
+    os.Exit(1)
+  }
 }
