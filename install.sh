2916e4be9e3d529dc7de69d90fab5d10901ccdd9
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-08-05 21:38:24 -0500

    Add install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+VERSION="0.1.4"
+URL="https://github.com/sosedoff/lunchy-go/releases/download/v${VERSION}/lunchy"
+BIN_PATH="/usr/local/bin/lunchy"
+
+echo "Downloading an installing lunchy v${VERSION}"
+wget -q -O $BIN_PATH $URL && chmod +x $BIN_PATH
+echo "Done. Installed into ${BIN_PATH}"
